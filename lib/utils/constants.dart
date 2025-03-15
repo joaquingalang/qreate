356e6652303926b922077ebf7506f5bc7ef6bb6d
joaquingalang <galangjoaquin@gmail.com>
2025-03-16 01:38:52 +0800

    created constants file

---
+++ lib/utils/constants.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+// Image Assets
+const String kAppLogoPath = 'assets/images/qreate_logo.png';
+
+// BLUE COLORS
+const Color kBlueColor500 = Color(0xFF03045E);
+const Color kBlueColor400 = Color(0xFF0077B6);
+const Color kBlueColor300 = Color(0xFF00B4D8);
+const Color kBlueColor200 = Color(0xFF90E0EF);
+const Color kBlueColor100 = Color(0xFFCAF0F8);
+
+// GRAY COLORS
+const Color kGrayColor100 = Color(0xFFF5F5F5);
+
+// TEXT STYLES
+const TextStyle kOrbitronTitle = TextStyle(
+  fontFamily: 'Orbitron',
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+  fontSize: 44,
+);
+
+const TextStyle kSubtext36 = TextStyle(
+  fontSize: 36,
+  fontWeight: FontWeight.w700,
+);
+
+const TextStyle kSubtext20 = TextStyle(
+  fontSize: 20,
+);
+
+const TextStyle kSubtext16 = TextStyle(
+  fontSize: 16,
+);
