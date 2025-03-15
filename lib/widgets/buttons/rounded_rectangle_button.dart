cc3a57d50089864c4aa9d699127c905b316f0c9f
joaquingalang <galangjoaquin@gmail.com>
2025-03-16 01:39:20 +0800

    created rounded_rectangle_button.dart

---
+++ lib/widgets/buttons/rounded_rectangle_button.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:qreate/utils/constants.dart';
+
+class RoundedRectangleButton extends StatelessWidget {
+  const RoundedRectangleButton({
+    super.key,
+    required this.title,
+    this.borderRadius,
+    required this.onPressed,
+  });
+
+  final String title;
+  final BorderRadius? borderRadius;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: double.infinity,
+        height: 60,
+        decoration: BoxDecoration(
+          borderRadius: borderRadius ?? BorderRadius.circular(8),
+          gradient: LinearGradient(
+            colors: [
+              kBlueColor500,
+              kBlueColor400,
+            ],
+          ),
+        ),
+        child: Center(
+          child: Text(
+            title,
+            style: kSubtext20.copyWith(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
