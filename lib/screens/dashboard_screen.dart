b265255a75b4d76145f264048126c59bc411947f
joaquingalang <galangjoaquin@gmail.com>
2025-03-17 00:33:13 +0800

    added dashboard_screen.dart

---
+++ lib/screens/dashboard_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:qreate/utils/constants.dart';
+import 'package:qreate/widgets/cards/qr_card.dart';
+import 'package:qreate/widgets/buttons/gradient_floating_button.dart';
+
+class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  List<Widget> qrList = [
+    QRCard(),
+    QRCard(),
+    QRCard(),
+    QRCard(),
+    QRCard(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kGrayColor200,
+
+      // Page App Bar
+      appBar: AppBar(
+        backgroundColor: kBlueColor500,
+        title: Row(
+          spacing: 11,
+          children: [
+            Image.asset(kAppLogoPath, width: 30),
+            Text('Qreate', style: kOrbitronMedium),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.logout,
+              color: Colors.white,
+              size: 30,
+            ),
+          ),
+        ],
+      ),
+
+      // Page Body
+      body: ListView(
+        children: [
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: GridView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: qrList.length,
+              gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              itemBuilder: (context, index) {
+                return qrList[index];
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: GradientFloatingButton(
+        onPressed: () {},
+      ),
+    );
+  }
+}
