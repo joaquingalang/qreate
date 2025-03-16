320f25172c9b9b3bb1d1722d9dd6488eb40b9dad
joaquingalang <galangjoaquin@gmail.com>
2025-03-17 00:32:11 +0800

    added qr_card.dart

---
+++ lib/widgets/cards/qr_card.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:qreate/utils/constants.dart';
+
+class QRCard extends StatelessWidget {
+  const QRCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double cardWidth = (screenWidth - 75) / 2;
+    double cardHeight = cardWidth - 10;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              offset: Offset(0, 2),
+              blurRadius: 8,
+              spreadRadius: 0,
+            ),
+          ],
+        ),
+        child: Column(
+          children: [
+            Container(
+              width: cardWidth,
+              height: cardHeight * 0.70,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(8),
+                  topRight: Radius.circular(8),
+                ),
+              ),
+              child: Center(
+                child: Image.asset('assets/images/qreate_logo_gray.png', width: 80),
+              ),
+            ),
+            Opacity(
+              opacity: 0.6,
+              child: Container(
+                width: cardWidth,
+                height: cardHeight * 0.30,
+                decoration: BoxDecoration(
+                  color: kBlueColor400,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(8),
+                    bottomRight: Radius.circular(8),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '  Title',
+                      style: kSubtext16.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.more_vert,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
