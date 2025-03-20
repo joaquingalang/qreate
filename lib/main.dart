2c991a363c792e387ef6db62a54528e954b015c1
joaquingalang <galangjoaquin@gmail.com>
2025-03-20 22:12:02 +0800

    added image assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:qreate/screens/auth_screen.dart';
+import 'package:qreate/screens/results_screen.dart';
+import 'package:qreate/screens/testing_screen.dart';
 import 'package:qreate/screens/sign_in_screen.dart';
 import 'package:qreate/screens/sign_up_screen.dart';
-import 'package:qreate/screens/testing_screen.dart';
+import 'package:qreate/screens/auth_screen.dart';
+import 'package:qreate/screens/dashboard_screen.dart';
+import 'package:qreate/screens/create_qr_screen.dart';
+import 'package:qreate/screens/qr_screen.dart';
 
 void main() {
   runApp(Qreate());
@@ -14,7 +18,7 @@ class Qreate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AuthScreen(),
+      home: CreateQrScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
