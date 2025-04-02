3069f81a53d80ae6d34aa9b5160ef2a12d723ccc
joaquingalang <galangjoaquin@gmail.com>
2025-04-03 01:50:11 +0800

    initialize supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:qreate/screens/results_screen.dart';
-import 'package:qreate/screens/testing_screen.dart';
-import 'package:qreate/screens/sign_in_screen.dart';
-import 'package:qreate/screens/sign_up_screen.dart';
-import 'package:qreate/screens/auth_screen.dart';
-import 'package:qreate/screens/dashboard_screen.dart';
-import 'package:qreate/screens/create_qr_screen.dart';
-import 'package:qreate/screens/qr_screen.dart';
+import 'utils/secrets.dart';
+import 'package:qreate/services/auth/auth_manager.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  await Supabase.initialize(url: SUPABASE_URL, anonKey: SUPABASE_ANON_KEY);
   runApp(Qreate());
 }
 
@@ -18,7 +14,7 @@ class Qreate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: CreateQrScreen(),
+      home: AuthManager(),
       debugShowCheckedModeBanner: false,
     );
   }
