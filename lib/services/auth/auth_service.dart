fe269fd925ce35a591bdc49ec808eb6eb96424a9
joaquingalang <galangjoaquin@gmail.com>
2025-04-03 01:51:34 +0800

    created auth_service.dart

---
+++ lib/services/auth/auth_service.dart
@@ -0,0 +1,32 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class AuthService {
+
+  final SupabaseClient _supabase = Supabase.instance.client;
+
+  // TODO: Sign-in with email and password
+  Future<AuthResponse> signInWithEmailPassword({required String email, required String password}) async {
+    return await _supabase.auth.signInWithPassword(email: email, password: password);
+  }
+
+  // TODO: Sign-up with email and password
+  Future<AuthResponse> signUpWithEmailPassword({required String email, required String password}) async {
+    return await _supabase.auth.signUp(email: email, password: password);
+  }
+
+  // TODO: Sign-out
+  Future<void> signOut() async {
+    await _supabase.auth.signOut();
+  }
+
+  // TODO: Get user email
+  String? getCurrentUserEmail() {
+    final session = _supabase.auth.currentSession;
+    final user = session?.user;
+    return user?.email;
+  }
+
+  // TODO: Get session stream
+  Stream<AuthState> get authListener => _supabase.auth.onAuthStateChange;
+
+}
