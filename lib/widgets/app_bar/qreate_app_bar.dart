1202d79051f332cf8562328cbdf17e32849afd25
joaquingalang <galangjoaquin@gmail.com>
2025-04-03 01:52:13 +0800

    created qreate_app_bar.dart

---
+++ lib/widgets/app_bar/qreate_app_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:qreate/utils/constants.dart';
+
+class QreateAppBar extends StatelessWidget {
+  const QreateAppBar({
+    super.key,
+    required this.context,
+    required this.title,
+  });
+
+  final String title;
+  final BuildContext context;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      title: Text(
+        title,
+        style: kSubtext20.copyWith(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      leading: IconButton(
+        onPressed: () => Navigator.pop(context),
+        icon: Icon(
+          Icons.arrow_back,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
