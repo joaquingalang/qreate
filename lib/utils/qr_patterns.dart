0d3d597f0050b173a3cb8e6092401f6a35556e2e
joaquingalang <galangjoaquin@gmail.com>
2025-04-03 03:44:29 +0800

    added pattern string map

--- lib/utils/qr_patterns.dart
+++ lib/utils/qr_patterns.dart
@@ -5,3 +5,19 @@ enum QrPattern {
   smooth,
   circles,
 }
+
+final Map<QrPattern, String> patternToString = {
+  QrPattern.classic: 'classic',
+  QrPattern.rounded: 'rounded',
+  QrPattern.thin: 'thin',
+  QrPattern.smooth: 'smooth',
+  QrPattern.circles: 'circles',
+};
+
+final Map<String, QrPattern> stringToPattern = {
+  'classic': QrPattern.classic,
+  'rounded': QrPattern.rounded,
+  'thin': QrPattern.thin,
+  'smooth': QrPattern.smooth,
+  'circles': QrPattern.circles,
+};
