c7510a956b58b79573387ea60ea04148e24da45c
joaquingalang <galangjoaquin@gmail.com>
2025-04-09 02:54:44 +0800

    testing connectivity service

--- lib/screens/testing_screen.dart
+++ lib/screens/testing_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:qreate/services/connectivity/connectivity_service.dart';
 import 'package:qreate/utils/constants.dart';
 import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:qreate/widgets/buttons/color_picker_button.dart';
@@ -11,99 +14,69 @@ class TestingScreen extends StatefulWidget {
 }
 
 class _TestingScreenState extends State<TestingScreen> {
-  late Color screenPickerColor;
-  late Color dialogPickerColor;
-  late Color dialogSelectColor;
+  final ConnectivityService _connectivity = ConnectivityService();
 
-  Future<bool> colorPickerDialog() async {
-    return ColorPicker(
-      // Use the dialogPickerColor as start and active color.
-      color: dialogPickerColor,
-      // Update the dialogPickerColor using the callback.
-      onColorChanged: (Color color) =>
-          setState(() => dialogPickerColor = color),
-      width: 40,
-      height: 40,
-      borderRadius: 4,
-      spacing: 5,
-      runSpacing: 5,
-      wheelDiameter: 155,
-      heading: Text(
-        'Select color',
-        style: Theme.of(context).textTheme.titleSmall,
-      ),
-      subheading: Text(
-        'Select color shade',
-        style: Theme.of(context).textTheme.titleSmall,
-      ),
-      wheelSubheading: Text(
-        'Selected color and its shades',
-        style: Theme.of(context).textTheme.titleSmall,
-      ),
-      // showMaterialName: true,
-      // showColorName: true,
-      // showColorCode: true,
-      // copyPasteBehavior: const ColorPickerCopyPasteBehavior(
-      //   longPressMenu: true,
-      // ),
-      materialNameTextStyle: Theme.of(context).textTheme.bodySmall,
-      colorNameTextStyle: Theme.of(context).textTheme.bodySmall,
-      colorCodeTextStyle: Theme.of(context).textTheme.bodySmall,
-      pickersEnabled: const <ColorPickerType, bool>{
-        ColorPickerType.both: false,
-        ColorPickerType.primary: true,
-        ColorPickerType.accent: true,
-        ColorPickerType.bw: false,
-        ColorPickerType.custom: true,
-        ColorPickerType.wheel: true,
-      },
-      // customColorSwatchesAndNames: colorsNameMap,
-    ).showPickerDialog(
-      context,
-      // New in version 3.0.0 custom transitions support.
-      transitionBuilder: (BuildContext context, Animation<double> a1,
-          Animation<double> a2, Widget widget) {
-        final double curvedValue =
-            Curves.easeInOutBack.transform(a1.value) - 1.0;
-        return Transform(
-          transform: Matrix4.translationValues(0.0, curvedValue * 200, 0.0),
-          child: Opacity(
-            opacity: a1.value,
-            child: widget,
-          ),
-        );
-      },
-      transitionDuration: const Duration(milliseconds: 400),
-      constraints:
-          const BoxConstraints(minHeight: 400, minWidth: 300, maxWidth: 320),
-    );
-  }
+  bool? _connection;
 
   @override
   void initState() {
     super.initState();
-    screenPickerColor = Colors.red;
-    dialogPickerColor = Colors.blue;
-    dialogSelectColor = const Color(0xFFA239CA);
+    StreamSubscription subscription = _connectivity.getSubscription((List<ConnectivityResult> result) {
+      setState(() {
+        if (result.contains(ConnectivityResult.none)) {
+          _connection = false;
+        } else {
+          _connection = true;
+        }
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          ColorPickerButton(
-            title: 'Canvas',
-            color: dialogPickerColor,
-            onColorChanged: (Color color) {
-              setState(
-                () {
-                  dialogPickerColor = color;
-                },
-              );
-            },
-          )
-        ],
+      body: Center(
+        child: Column(
+          spacing: 20,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: EdgeInsets.all(15),
+              color: _connection! ? Colors.green : Colors.red,
+              child: Text(
+                _connection!
+                    ? 'Connected to internet'
+                    : 'No internet connection',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () async {
+                final bool isConnected =
+                    await _connectivity.checkConnectivity();
+                print('CONNECTION STATUS: $isConnected');
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    backgroundColor: isConnected ? Colors.green : Colors.red,
+                    content: Text(
+                      isConnected
+                          ? 'Connected to internet'
+                          : 'No internet connection',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                );
+              },
+              child: Text('Check Connectivity'),
+            ),
+          ],
+        ),
       ),
     );
   }
